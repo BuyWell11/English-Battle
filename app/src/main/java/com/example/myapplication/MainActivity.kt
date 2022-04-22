f3acf03a086dfcc3418d246b7e508cdee6067a36
BuyWell <maksim-d2013@mail.ru>
2022-04-22 17:09:10 +0300

    add libs

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,25 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.MainMenuBinding
+import okhttp3.*
+import java.io.IOException
 import java.sql.ResultSet
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: MainMenuBinding
+    private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val db  = Database()
-        var rs: ResultSet
-        object : Thread() {
-            override fun run() {
-                rs = db.GetTaskLDS()
-            }
-        }.start()
+        //val db  = Database()
+        run("http://217.25.230.151:3000")
         val a = 1
 
 
@@ -33,6 +32,17 @@ class MainActivity : AppCompatActivity() {
         binding.btn3.setOnClickListener{
             onExitPressed()
         }
+    }
+
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+        })
     }
 
     private fun onPlayPressed(){
