1b8ea9263c8960a3ad0c0ee4310a59f5be2c853c
Dmitry M <matovxd@gmail.com>
2022-05-10 04:04:52 +0300

    add history for users

--- app/src/main/java/com/example/myapplication/database/DatabaseManager.kt
+++ app/src/main/java/com/example/myapplication/database/DatabaseManager.kt
@@ -9,9 +9,37 @@ class DatabaseManager(val context : Context) {
     val dbHelper = DatabaseHelper(context)
     var db : SQLiteDatabase? = null
 
+    var currentUserID : Int = 1
+
     fun openDB()
     {
         db = dbHelper.writableDatabase
+    }
+
+    fun updateHistory(currentTask : Int, task_type : Int)
+    {
+        var columnName : String? = null
+
+        when(task_type)
+        {
+            1 -> columnName = DatabaseNames.HISTORY_COLUMN_LDS_WORD_HISTORY
+            2 -> columnName = DatabaseNames.HISTORY_COLUMN_LDS_PICTURE_HISTORY
+            3 -> columnName = DatabaseNames.HISTORY_COLUMN_MDS_WORD_HISTORY
+            4 -> columnName = DatabaseNames.HISTORY_COLUMN_MDS_SENTENCE_HISTORY
+            5 -> columnName = DatabaseNames.HISTORY_COLUMN_HDS_WORD_HISTORY
+            6 -> columnName = DatabaseNames.HISTORY_COLUMN_HDS_TRANSLATION_HISTORY
+        }
+
+        val values = ContentValues().apply {
+            put(columnName, currentTask + 1)
+        }
+
+        val count = db?.update(
+            DatabaseNames.HISTORY_TABLE_NAME,
+            values,
+            "${DatabaseNames.HISTORY_COLUMN_ID} LIKE ?",
+            arrayOf(currentUserID.toString()),
+        )
     }
 
     fun insertIntoUsersTable(user_name : String, user_password : String, user_email : String)
@@ -23,6 +51,15 @@ class DatabaseManager(val context : Context) {
         }
 
         db?.insert(DatabaseNames.USER_TABLE_NAME, null, values)
+    }
+
+    fun insertIntoHistoryTable(history_id : Int)
+    {
+        val values = ContentValues().apply {
+            put(DatabaseNames.HISTORY_COLUMN_ID, history_id)
+        }
+
+        db?.insert(DatabaseNames.HISTORY_TABLE_NAME, null, values)
     }
 
     @SuppressLint("Range")
@@ -40,11 +77,16 @@ class DatabaseManager(val context : Context) {
         val dataList = ArrayList<String>()
         while (cursor?.moveToNext()!!)
         {
-            val temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.USER_COLUMN_PASSWORD))
+            var temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.USER_COLUMN_PASSWORD))
+            dataList.add(temp.toString())
+
+            temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.USER_COLUMN_ID))
             dataList.add(temp.toString())
         }
 
         if (dataList.size != 0 && dataList[0] == user_password) {
+            currentUserID = dataList[1].toInt()
+            insertIntoHistoryTable(currentUserID)
             cursor.close()
             return true
         }
@@ -56,12 +98,14 @@ class DatabaseManager(val context : Context) {
     @SuppressLint("Range")
     fun GetLdsWordInfo() : ArrayList<String>
     {
+        val currentTask = GetCurrentTask(1)
+
         val dataList = ArrayList<String>()
         val cursor = db?.query(
             DatabaseNames.LDS_WORD_TABLE_NAME,
             null,
-            "${DatabaseNames.LDS_WORD_COLUMN_ID} = 1",
-            null,
+            "${DatabaseNames.LDS_WORD_COLUMN_ID} = ?",
+            arrayOf(currentTask.toString()),
             null,
             null,
             null
@@ -81,18 +125,22 @@ class DatabaseManager(val context : Context) {
 
         cursor.close()
 
+        updateHistory(currentTask, 1)
+
         return dataList
     }
 
     @SuppressLint("Range")
     fun GetLdsPictureInfo() : ArrayList<String>
     {
+        val currentTask = GetCurrentTask(2)
+
         val dataList = ArrayList<String>()
         val cursor = db?.query(
             DatabaseNames.LDS_PICTURE_TABLE_NAME,
             null,
-            "${DatabaseNames.LDS_PICTURE_COLUMN_ID} = 1",
-            null,
+            "${DatabaseNames.LDS_PICTURE_COLUMN_ID} = ?",
+            arrayOf(currentTask.toString()),
             null,
             null,
             null
@@ -112,33 +160,7 @@ class DatabaseManager(val context : Context) {
 
         cursor.close()
 
-        return dataList
-    }
-
-    @SuppressLint("Range")
-    fun GetMdsMakeSentenceInfo() : ArrayList<String>
-    {
-        val dataList = ArrayList<String>()
-        val cursor = db?.query(
-            DatabaseNames.MDS_MAKE_SENTENCE_TABLE_NAME,
-            null,
-            "${DatabaseNames.MDS_MAKE_SENTENCE_COLUMN_ID} = 1",
-            null,
-            null,
-            null,
-            null
-        )
-
-        while(cursor?.moveToNext()!!)
-        {
-            var temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.MDS_MAKE_SENTENCE_COLUMN_WORD_LIST))
-            dataList.add(temp.toString())
-
-            temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.MDS_MAKE_SENTENCE_COLUMN_RIGHT_ANSWER))
-            dataList.add(temp.toString())
-        }
-
-        cursor.close()
+        updateHistory(currentTask, 2)
 
         return dataList
     }
@@ -146,12 +168,14 @@ class DatabaseManager(val context : Context) {
     @SuppressLint("Range")
     fun GetMdsMakeWordInfo() : ArrayList<String>
     {
+        val currentTask = GetCurrentTask(3)
+
         val dataList = ArrayList<String>()
         val cursor = db?.query(
             DatabaseNames.MDS_MAKE_WORD_TABLE_NAME,
             null,
-            "${DatabaseNames.MDS_MAKE_WORD_COLUMN_ID} = 1",
-            null,
+            "${DatabaseNames.MDS_MAKE_WORD_COLUMN_ID} = ?",
+            arrayOf(currentTask.toString()),
             null,
             null,
             null
@@ -171,18 +195,54 @@ class DatabaseManager(val context : Context) {
 
         cursor.close()
 
+        updateHistory(currentTask, 3)
+
+        return dataList
+    }
+
+    @SuppressLint("Range")
+    fun GetMdsMakeSentenceInfo() : ArrayList<String>
+    {
+        val currentTask = GetCurrentTask(4)
+
+        val dataList = ArrayList<String>()
+        val cursor = db?.query(
+            DatabaseNames.MDS_MAKE_SENTENCE_TABLE_NAME,
+            null,
+            "${DatabaseNames.MDS_MAKE_SENTENCE_COLUMN_ID} = ?",
+            arrayOf(currentTask.toString()),
+            null,
+            null,
+            null
+        )
+
+        while(cursor?.moveToNext()!!)
+        {
+            var temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.MDS_MAKE_SENTENCE_COLUMN_WORD_LIST))
+            dataList.add(temp.toString())
+
+            temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.MDS_MAKE_SENTENCE_COLUMN_RIGHT_ANSWER))
+            dataList.add(temp.toString())
+        }
+
+        cursor.close()
+
+        updateHistory(currentTask, 4)
+
         return dataList
     }
 
     @SuppressLint("Range")
     fun GetHdsChooseWordInfo() : ArrayList<String>
     {
+        val currentTask = GetCurrentTask(5)
+
         val dataList = ArrayList<String>()
         val cursor = db?.query(
             DatabaseNames.HDS_CHOOSE_WORD_TABLE_NAME,
             null,
-            "${DatabaseNames.HDS_CHOOSE_WORD_COLUMN_ID} = 1",
-            null,
+            "${DatabaseNames.HDS_CHOOSE_WORD_COLUMN_ID} = ?",
+            arrayOf(currentTask.toString()),
             null,
             null,
             null
@@ -202,18 +262,22 @@ class DatabaseManager(val context : Context) {
 
         cursor.close()
 
+        updateHistory(currentTask, 5)
+
         return dataList
     }
 
     @SuppressLint("Range")
     fun GetHdsTranslationInfo() : ArrayList<String>
     {
+        val currentTask = GetCurrentTask(6)
+
         val dataList = ArrayList<String>()
         val cursor = db?.query(
             DatabaseNames.HDS_TRANSLATION_TABLE_NAME,
             null,
-            "${DatabaseNames.HDS_TRANSLATION_COLUMN_ID} = 1",
-            null,
+            "${DatabaseNames.HDS_TRANSLATION_COLUMN_ID} = ?",
+            arrayOf(currentTask.toString()),
             null,
             null,
             null
@@ -230,7 +294,44 @@ class DatabaseManager(val context : Context) {
 
         cursor.close()
 
+        updateHistory(currentTask, 6)
+
         return dataList
+    }
+
+    @SuppressLint("Range")
+    fun GetCurrentTask(task_type : Int) : Int
+    {
+        val dataList = ArrayList<String>()
+        val cursor = db?.query(
+            DatabaseNames.HISTORY_TABLE_NAME,
+            null,
+            "${DatabaseNames.HISTORY_COLUMN_ID} = ?",
+            arrayOf(currentUserID.toString()),
+            null,
+            null,
+            null
+        )
+
+        while(cursor?.moveToNext()!!)
+        {
+            var temp : String? = null
+
+            when(task_type)
+            {
+                1 -> temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.HISTORY_COLUMN_LDS_WORD_HISTORY))
+                2 -> temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.HISTORY_COLUMN_LDS_PICTURE_HISTORY))
+                3 -> temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.HISTORY_COLUMN_MDS_WORD_HISTORY))
+                4 -> temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.HISTORY_COLUMN_MDS_SENTENCE_HISTORY))
+                5 -> temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.HISTORY_COLUMN_HDS_WORD_HISTORY))
+                6 -> temp = cursor.getString(cursor.getColumnIndex(DatabaseNames.HISTORY_COLUMN_HDS_TRANSLATION_HISTORY))
+            }
+
+            dataList.add(temp.toString())
+        }
+
+        cursor.close()
+        return dataList[0].toInt()
     }
 
     fun closeDB()
