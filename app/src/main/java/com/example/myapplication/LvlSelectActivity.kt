87ff211f6d8f98cfc77978d757e7779e64ef183a
Dmitry M <matovxd@gmail.com>
2022-05-20 19:36:49 +0300

    add current level history prototype

--- app/src/main/java/com/example/myapplication/LvlSelectActivity.kt
+++ app/src/main/java/com/example/myapplication/LvlSelectActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.myapplication.database.DatabaseManager
 import com.example.myapplication.databinding.LvlSelectBinding
 import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.lvl_select.*
 
 class LvlSelectActivity : AppCompatActivity() {
     lateinit var binding: LvlSelectBinding
@@ -14,6 +16,16 @@ class LvlSelectActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = LvlSelectBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val dbManager = DatabaseManager(this)
+        dbManager.openDB()
+
+        val currentLvl = dbManager.GetCurrentLvl()
+
+        dbManager.closeDB()
+
+        chooseCurrentLvl(currentLvl)
+
         binding.btn1.setOnClickListener{
             onLvl(1)
         }
@@ -29,5 +41,15 @@ class LvlSelectActivity : AppCompatActivity() {
         val intent = Intent(this, FightActivity::class.java)
         intent.putExtra(FightActivity.ENEMY_PICS, lvl)
         startActivity(intent)
+    }
+
+    private fun chooseCurrentLvl(currentLvl : Int)
+    {
+        when(currentLvl)
+        {
+            1 -> btn1.isEnabled = true
+            2 -> btn2.isEnabled = true
+            3 -> btn3.isEnabled = true
+        }
     }
 }
