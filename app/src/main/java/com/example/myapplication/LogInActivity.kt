57b83a06c59a022ccfc59f34c2327358b10722a2
BuyWell <maksim-d2013@mail.ru>
2022-05-30 19:22:59 +0300

    add CD

--- app/src/main/java/com/example/myapplication/LogInActivity.kt
+++ app/src/main/java/com/example/myapplication/LogInActivity.kt
@@ -3,7 +3,7 @@ package com.example.myapplication
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
+    import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
